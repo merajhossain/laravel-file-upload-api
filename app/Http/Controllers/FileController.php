f3e223d37f0f2615b340c92dd67160d1159f9d6a
Hossain <merajhossain29@gmail.com>
2023-12-07 00:39:27 +0600

    init commit

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Log;
+use App\Models\File;
+
+class FileController extends Controller
+{
+    public function upload(Request $request)
+    {
+        Log::info('Upload function started');
+        $uploadsPath = 'public/uploads';
+        if (!Storage::exists($uploadsPath)) {
+            Storage::makeDirectory($uploadsPath);
+        }
+        try {
+            $files = $request->file('files');
+            foreach ($files as $file) {
+                $filename = $file->getClientOriginalName();
+                $path = $file->storeAs('uploads', $filename, 'local');
+                File::create([
+                    'name' => $filename,
+                    'path' => $path,
+                ]);
+            }
+        } catch (\Exception $e) {
+            Log::error('File upload error: ' . $e->getMessage());
+            return response()->json(['error' => 'File upload failed'], 500);
+        }
+
+        Log::info('Upload function completed successfully');
+        return response()->json(['message' => 'Files uploaded successfully']);
+    }
+}
